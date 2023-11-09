178a06703365f4cb18c267a81f85d0a724e98581
michaelcantprogram <changdao.he@mail.utoronto.ca>
2023-11-09 01:37:58 -0500

    Changed the number of required parameters in getCityWeather method. The date is required now.

--- src/use_case/weather/GetWeatherDataAccessInterface.java
+++ src/use_case/weather/GetWeatherDataAccessInterface.java
@@ -1,5 +1,7 @@
 package use_case.weather;
 
+import entity.Weather;
+
 public interface GetWeatherDataAccessInterface {
-    Weather getCityWeather(String city);
+    Weather getCityWeather(String city, String date);
 }
