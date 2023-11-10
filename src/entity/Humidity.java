19d4a7cffbda99328c747c82b62a052a331870f3
hondapowerdd <william.meng@mail.utornto.ca>
2023-11-09 23:16:50 -0500

    write some entity

--- src/entity/Humidity.java
+++ src/entity/Humidity.java
@@ -1,6 +1,19 @@
 package entity;
 
 public class Humidity {
+    private double humidity;
 
+    public Humidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public void setHumidity(double humidity) {
+        this.humidity = humidity;
+    }
+
+    public double getHumidity() {
+        return this.humidity;
+    }
 }
+
 
--- src/use_case/weather/weatherDataAccessInterface.java
+++ src/use_case/weather/weatherDataAccessInterface.java
@@ -1,5 +1,7 @@
 package use_case.weather;
 
+import entity.Weather;
+
 public interface weatherDataAccessInterface {
     Weather getCityWeather(String city);
 }
