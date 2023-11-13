a9bbee4026905f85e844ec7ef1c66ea5f2583ba7
junwei0102 <junwei.quan@mail.utoronto.ca>
2023-11-13 13:57:21 -0500

    use case and entity for the function of change unit

---
+++ src/use_case/change_unit/ChangeUnitDataAccessInterface.java
@@ -0,0 +1,7 @@
+package use_case.change_unit;
+import entity.TemperatureUnit;
+
+public interface ChangeUnitDataAccessInterface {
+    void saveTemperatureUnit(TemperatureUnit unit);
+    TemperatureUnit getTemperatureUnit();
+}
---
+++ src/use_case/change_unit/ChangeUnitInputData.java
@@ -0,0 +1,15 @@
+package use_case.change_unit;
+import entity.TemperatureUnit;
+
+public class ChangeUnitInputData {
+    private final TemperatureUnit.Unit unit;
+
+    public ChangeUnitInputData(TemperatureUnit.Unit unit) {
+        this.unit = unit;
+    }
+
+    public TemperatureUnit.Unit getUnit() {
+        return unit;
+    }
+}
+
