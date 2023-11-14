e4487c789861569752ad35e72891bee4eed0d4c3
michaelcantprogram <changdao.he@mail.utoronto.ca>
2023-11-13 22:06:09 -0500

    Modified the implementation of files in GetWeather use-case

--- src/use_case/get_weather/GetWeatherInputBoundary.java
+++ src/use_case/get_weather/GetWeatherInputBoundary.java
@@ -1,5 +1,7 @@
 package use_case.get_weather;
 
+import entity.Weather;
+
 public interface GetWeatherInputBoundary {
     void execute(GetWeatherInputData getWeatherInputData);
 }
--- src/use_case/get_weather/GetWeatherInputData.java
+++ src/use_case/get_weather/GetWeatherInputData.java
@@ -1,16 +1,12 @@
 package use_case.get_weather;
 
+import entity.Weather;
 import java.time.LocalDate;
 
 public class GetWeatherInputData {
 
     final private String city;
     final private String date;
-
-    public GetWeatherInputData(String city, String date) {
-        this.city = city;
-        this.date = date;
-    }
 
     public GetWeatherInputData(String city) {
         this.city = city;
