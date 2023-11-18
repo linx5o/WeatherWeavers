870f7c33f7b3e023f778530094686895b8bdf860
EL5 <linedison3@gmail.com>
2023-11-18 11:23:59 +0800

    started view for modules of Weather

--- src/view/weather/HourlyView.java
+++ src/view/weather/HourlyView.java
@@ -1,2 +1,32 @@
-package view.Weather;public class HourlyView {
+package view.Weather;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+public class HourlyView extends JPanel implements ActionListener, PropertyChangeListener {
+    public final String viewName = "hourly";
+
+    private final HourlyController controller;
+    private final HourlyViewModel hourlyViewModel;
+
+    public HourlyView(HourlyController controller, HourlyViewModel hourlyViewModel) {
+        this.controller = controller;
+        this.hourlyViewModel = hourlyViewModel;
+        hourlyViewModel.addPropertyChangeListener(this);
+
+        HourlyState state = hourlyViewModel.getState();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+
+    }
 }
