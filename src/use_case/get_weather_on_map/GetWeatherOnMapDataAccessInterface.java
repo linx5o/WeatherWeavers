1dace859d65958050b95864fe6d46cb97a9cb131
hondapowerdd <william.meng@mail.utornto.ca>
2023-11-20 15:54:47 -0500

    complete the usecase part of get weather on map use case.

--- src/use_case/get_weather_on_map/GetWeatherOnMapDataAccessInterface.java
+++ src/use_case/get_weather_on_map/GetWeatherOnMapDataAccessInterface.java
@@ -1,4 +1,7 @@
 package use_case.get_weather_on_map;
 
+import entity.Weather;
+
 public interface GetWeatherOnMapDataAccessInterface {
+    Weather getWeatherByName(String cityname, Boolean isCelsius);
 }
--- src/use_case/get_weather_on_map/GetWeatherOnMapOutputData.java
+++ src/use_case/get_weather_on_map/GetWeatherOnMapOutputData.java
@@ -1,4 +1,15 @@
 package use_case.get_weather_on_map;
 
+import entity.Weather;
+
 public class GetWeatherOnMapOutputData {
+    private final Weather weather;
+
+    public GetWeatherOnMapOutputData(Weather weather) {
+        this.weather = weather;
+    }
+
+    public Weather getWeather() {
+        return weather;
+    }
 }
