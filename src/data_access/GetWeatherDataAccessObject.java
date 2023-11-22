b704c5d1848eaaa7a5911d0580f0b9e356787543
EL5 <linedison3@gmail.com>
2023-11-22 10:12:43 -0500

    added dependencies

--- src/data_access/GetWeatherDataAccessObject.java
+++ src/data_access/GetWeatherDataAccessObject.java
@@ -1,11 +1,44 @@
 package data_access;
 
 import entity.Weather;
+import org.json.JSONException;
+import org.json.JSONObject;
 import use_case.get_weather.GetWeatherDataAccessInterface;
+import okhttp3.*;
+
+import java.io.IOException;
 
 public class GetWeatherDataAccessObject implements GetWeatherDataAccessInterface {
+    private static final String API_FORECAST = "https://api.aerisapi.com/forecast/";
+    private static final String API_CURRENT = "https://api.aerisapi.com/current/";
+    // load API_TOKEN from env variable.
+    private static final String API_TOKEN = System.getenv("API_TOKEN");
+
+    public static String getApiToken() {
+        return API_TOKEN;
+    }
+
     @Override
     public Weather getCityWeather(String city, String date, Boolean celsius) {
-        return null;
+        OkHttpClient client = new OkHttpClient().newBuilder()
+                .build();
+        Request request = new Request.Builder()
+                .url(String.format("https://api.aerisapi.com/forecast/%s?format=json&filter=day&limit=1&client_id=%s", city, API_TOKEN))
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            System.out.println(response);
+            JSONObject responseBody = new JSONObject(response.body().string());
+
+            if (responseBody.getBoolean("success")) {
+                JSONObject responseData = responseBody.getJSONArray("response").getJSONObject(0);
+
+
+            } else {
+                throw new RuntimeException(responseBody.getString("error"));
+            }
+        } catch (IOException | JSONException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/interface_adapter/GetWeather/GetWeatherPresenter.java
+++ src/interface_adapter/GetWeather/GetWeatherPresenter.java
@@ -6,7 +6,9 @@ import use_case.get_weather.GetWeatherOutputData;
 import javax.swing.*;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public class GetWeatherPresenter implements GetWeatherOutputBoundary {
@@ -22,41 +24,24 @@ public class GetWeatherPresenter implements GetWeatherOutputBoundary {
         state.setCity(getWeatherOutputData.getCity());
         state.setDate(getWeatherOutputData.getWeekDay() + ", " + getWeatherOutputData.getDate());
 
-        Set likely = new HashSet();
-        likely.add("AR:")
+        if (getWeatherOutputData.getDescriptions().equals("sunny")) {
+            state.setDescription("Sunny");
+            state.setTemperatureIcon(new ImageIcon("src/interface_adapter/GetWeather/icons/sunny.png"));
+        } else if (getWeatherOutputData.getDescriptions().equals("clear")) {
+            state.setDescription("Clear");
+            state.setTemperatureIcon(new ImageIcon("src/interface_adapter/GetWeather/icons/clear.png"));
+        } else {
+            state.setDescription("Unknown");
+            state.setTemperatureIcon(new ImageIcon("src/interface_adapter/GetWeather/icons/unknown.png"));
+        }
 
-        int sunrise = Integer.parseInt(getWeatherOutputData.getSunrise());
-        int sunset = Integer.parseInt(getWeatherOutputData.getSunset());
-        LocalTime now = LocalTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HHmm");
-        int formattedTime = Integer.parseInt(now.format(formatter));
-
-        if (getWeatherOutputData.getDescriptions().equals("::CL")) {
-            if (formattedTime > sunrise && formattedTime < sunset) {
-                state.setDescription("Sunny");
-                state.setTemperatureIcon(new ImageIcon("src/interface_adapter/GetWeather/icons/sunny.png"));
-            } else {
-                state.setDescription("Clear");
-                state.setTemperatureIcon(new ImageIcon("src/interface_adapter/GetWeather/icons/clear.png"));
-            }
-        } else if (getWeatherOutputData.getDescriptions().equals("::FW") || getWeatherOutputData.getDescriptions().equals("::SC")) {
-            state.setDescription("Partly Cloudy");
-            if (formattedTime > sunrise && formattedTime < sunset) {
-                state.setTemperatureIcon(new ImageIcon("src/interface_adapter/GetWeather/icons/partly_cloudy.png"));
-            } else {
-                state.setTemperatureIcon(new ImageIcon("src/interface_adapter/GetWeather/icons/partly_cloudy_night.png"));
-            }
-        } else if (getWeatherOutputData.getDescriptions().equals("::BK")) {
-            state.setDescription("Mostly Cloudy");
-            if (formattedTime > sunrise && formattedTime < sunset) {
-                state.setTemperatureIcon(new ImageIcon("src/interface_adapter/GetWeather/icons/mostly_cloudy.png"));
-            } else {
-                state.setTemperatureIcon(new ImageIcon("src/interface_adapter/GetWeather/icons/mostly_cloudy_night.png"));
-            }
-        } else if (getWeatherOutputData.getDescriptions().equals("::OV")) {
-            state.setDescription("Cloudy");
-            state.setTemperatureIcon(new ImageIcon("src/interface_adapter/GetWeather/icons/cloudy.png"));
-        } else if (getWeatherOutputData.getDescriptions())
+        state.setTemperature(getWeatherOutputData.getCurrentTemp() + "°");
+        state.setHigh(getWeatherOutputData.getHighTemp() + "°");
+        state.setLow(getWeatherOutputData.getLowTemp() + "°");
+        state.setWind(getWeatherOutputData.getWindSpeed() + " KM/H");
+        state.setRain(getWeatherOutputData.getRainPercent() + "%");
+        state.setSunrise(getWeatherOutputData.getSunrise());
+        state.setSunset(getWeatherOutputData.getSunset());
     }
 
     @Override
