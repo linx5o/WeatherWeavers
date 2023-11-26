7185dfa0175cdc5e395387f24aa7315a5bf2188b
EL5 <linedison3@gmail.com>
2023-11-27 07:28:11 +0800

    updated Humidity to take integer

--- src/use_case/humidity/HumidityInteractor.java
+++ src/use_case/humidity/HumidityInteractor.java
@@ -1,18 +1,33 @@
 package use_case.humidity;
 
 import entity.Humidity;
+import use_case.get_weather.GetSettingsDataAccessInterface;
+
+import java.time.LocalTime;
 
 public class HumidityInteractor implements HumidityInputBoundary{
     final HumidityDataAccessInterface humidityDataAccessObject;
     final HumidityOutputBoundary humidityPresenter;
-    public HumidityInteractor(HumidityDataAccessInterface humidityDataAccessObject, HumidityOutputBoundary humidityOutputBoundary) {
+    final GetSettingsDataAccessInterface getSettingsDataAccessObject;
+    public HumidityInteractor(HumidityDataAccessInterface humidityDataAccessObject, HumidityOutputBoundary humidityOutputBoundary, GetSettingsDataAccessInterface getSettingsDataAccessObject) {
         this.humidityDataAccessObject = humidityDataAccessObject;
         this.humidityPresenter = humidityOutputBoundary;
+        this.getSettingsDataAccessObject = getSettingsDataAccessObject;
     }
 
     @Override
     public void execute(HumidityInputData humidityInputData) {
         Humidity humidity = humidityDataAccessObject.getHumidity(humidityInputData.getCity(), humidityInputData.getTime());
+        if (humidity == null) {
+            humidityPresenter.prepareFailView("Error");
+        } else {
+            humidityPresenter.prepareSuccessView(new HumidityOutputData(humidity));
+        }
+    }
+
+    @Override
+    public void execute(LocalTime localTime) {
+        Humidity humidity = humidityDataAccessObject.getHumidity(getSettingsDataAccessObject.getSettings().getDefaultCity(), localTime.toString());
         if (humidity == null) {
             humidityPresenter.prepareFailView("Error");
         } else {
