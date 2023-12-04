b7ee364c5145d4ffe79767f84c256c27c86e1bfe
EL5 <linedison3@gmail.com>
2023-12-04 15:46:48 -0500

    updated state

--- src/interface_adapter/Settings/SettingsState.java
+++ src/interface_adapter/Settings/SettingsState.java
@@ -1,5 +1,34 @@
 package interface_adapter.Settings;
 
 public class SettingsState {
+    private String city;
+    private boolean tempUnit;
+    private boolean is24HourFormat;
 
+    public SettingsState() {
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public boolean getTempUnit() {
+        return tempUnit;
+    }
+
+    public boolean getIs24HourFormat() {
+        return is24HourFormat;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setTempUnit(boolean tempUnit) {
+        this.tempUnit = tempUnit;
+    }
+
+    public void setTimeFormat(boolean is24HourFormat) {
+        this.is24HourFormat = is24HourFormat;
+    }
 }
