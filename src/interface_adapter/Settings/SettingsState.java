940c05a87a583ee3018daabb392d7403a3554f0d
EL5 <linedison3@gmail.com>
2023-12-04 16:16:43 -0500

    updated settings state

--- src/interface_adapter/Settings/SettingsState.java
+++ src/interface_adapter/Settings/SettingsState.java
@@ -7,8 +7,62 @@ public class SettingsState {
     private String listCityMessage;
     private boolean tempUnit;
     private boolean is24HourFormat;
+    private String listCityCity;
+    private String listCityCountry;
+    private String defaultCityMessage;
+    private String defaultCity;
+    private String defaultCityCity;
+    private String defaultCityCountry;
 
     public SettingsState() {
+    }
+
+    public String getListCityCity() {
+        return listCityCity;
+    }
+
+    public void setListCityCity(String listCityCity) {
+        this.listCityCity = listCityCity;
+    }
+
+    public String getListCityCountry() {
+        return listCityCountry;
+    }
+
+    public void setListCityCountry(String listCityCountry) {
+        this.listCityCountry = listCityCountry;
+    }
+
+    public String getDefaultCityMessage() {
+        return defaultCityMessage;
+    }
+
+    public void setDefaultCityMessage(String defaultCityMessage) {
+        this.defaultCityMessage = defaultCityMessage;
+    }
+
+    public String getDefaultCity() {
+        return defaultCity;
+    }
+
+    public void setDefaultCity(String defaultCity) {
+        this.defaultCity = defaultCity;
+    }
+
+    public String getDefaultCityCity() {
+        return defaultCityCity;
+    }
+
+    public void setDefaultCityCity(String defaultCityCity) {
+        this.defaultCityCity = defaultCityCity;
+    }
+
+    public String getDefaultCityCountry() {
+        return defaultCityCountry;
+    }
+
+    public void setDefaultCityCountry(String defaultCityCountry) {
+        this.defaultCityCountry = defaultCityCountry;
     }
 
     public void addListCity(String city) {
