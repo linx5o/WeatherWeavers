fef9e45f78c22517c48bf81b812dec965e070737
EL5 <linedison3@gmail.com>
2023-12-05 02:14:07 -0500

    added basic blocks for SettingsView

--- src/view/settings/SettingsView.java
+++ src/view/settings/SettingsView.java
@@ -5,8 +5,10 @@ import interface_adapter.SetCity.SetCityController;
 import interface_adapter.SetDefaultCity.SetDefaultCityController;
 import interface_adapter.SetTempUnit.SetTempUnitController;
 import interface_adapter.SetTimeFormat.SetTimeFormatController;
+import interface_adapter.Settings.SettingsState;
 import interface_adapter.Settings.SettingsViewModel;
 import interface_adapter.SettingsStart.SettingsStartController;
+import view.RoundedPanel;
 import view.ToggleSwitch;
 
 import javax.swing.*;
@@ -15,10 +17,15 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
 public class SettingsView extends JPanel implements ActionListener, PropertyChangeListener {
     public final String viewName = "settings";
 
+    private final JLabel defaultCityMessage = new JLabel();
+    private final JLabel cityMessage = new JLabel();
+    private final JLabel eraseCityMessage = new JLabel();
+    private final JList<String> cityList = new JList<>();
     private final JTextField defaultCityCityInputField = new JTextField(15);
     private final JTextField defaultCityCountryInputField = new JTextField(15);
     private final JTextField cityCityInputField = new JTextField(15);
@@ -53,6 +60,56 @@ public class SettingsView extends JPanel implements ActionListener, PropertyChan
         this.eraseCityButton = new JButton(settingsViewModel.ERASE_CITY_BUTTON_LABEL);
         this.tempUnitToggleSwitch = new ToggleSwitch();
         this.timeFormatToggleSwitch = new ToggleSwitch();
+        this.cityList.setVisibleRowCount(6);
+
+        JScrollPane scrollPane = new JScrollPane(this.cityList);
+        scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_NEVER);
+
+        this.setLayout(new GridBagLayout());
+        this.setPreferredSize(new Dimension(920, 620));
+        this.setBackground(Color.decode("#0D131D"));
+
+        this.addPropertyChangeListener(this);
+
+        settingsController.execute();
+
+        RoundedPanel defaultCityPanel = new RoundedPanel(40);
+        RoundedPanel togglesPanel = new RoundedPanel(40);
+        RoundedPanel cityListPanel = new RoundedPanel(40);
+
+        defaultCityPanel.setLayout(new GridBagLayout());
+        defaultCityPanel.setPreferredSize(new Dimension(520, 180));
+        defaultCityPanel.setBackground(Color.decode("#222B3A"));
+
+        togglesPanel.setLayout(new GridBagLayout());
+        togglesPanel.setPreferredSize(new Dimension(360, 180));
+        togglesPanel.setBackground(Color.decode("#222B3A"));
+
+        cityListPanel.setLayout(new GridBagLayout());
+        cityListPanel.setPreferredSize(new Dimension(900, 380));
+        cityListPanel.setBackground(Color.decode("#222B3A"));
+
+        GridBagConstraints constraints = new GridBagConstraints();
+
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(0, 0, 0, 0);
+
+        this.add(defaultCityPanel, constraints);
+
+        constraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.insets = new Insets(0, 20, 0, 20);
+
+        this.add(togglesPanel, constraints);
+
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        constraints.gridwidth = 2;
+        constraints.insets = new Insets(20, 0, 40, 20);
+
+        this.add(cityListPanel, constraints);
     }
 
     @Override
@@ -62,6 +119,18 @@ public class SettingsView extends JPanel implements ActionListener, PropertyChan
 
     @Override
     public void propertyChange(PropertyChangeEvent evt) {
-
+        SettingsState currentState = settingsViewModel.getState();
+        this.defaultCityCityInputField.setText(currentState.getDefaultCityCity());
+        this.defaultCityCountryInputField.setText(currentState.getDefaultCityCountry());
+        this.cityCityInputField.setText(currentState.getListCityCity());
+        this.cityCountryInputField.setText(currentState.getListCityCountry());
+        this.eraseCityCityInputField.setText(currentState.getEraseCityCity());
+        this.eraseCityCountryInputField.setText(currentState.getEraseCityCountry());
+        this.defaultCityMessage.setText(currentState.getDefaultCityMessage());
+        this.cityMessage.setText(currentState.getListCityMessage());
+        this.eraseCityMessage.setText(currentState.getEraseCityMessage());
+        this.tempUnitToggleSwitch.setSelected(currentState.getTempUnit());
+        this.timeFormatToggleSwitch.setSelected(currentState.getIs24HourFormat());
+        this.cityList.setListData(currentState.getCityList().toArray(new String[0]));
     }
 }
