ec2cf7b1b2ea770c538183ab6bf3db33e1359c3a
EL5 <linedison3@gmail.com>
2023-12-04 23:50:58 -0500

    prepared setting view

--- src/view/settings/SettingsView.java
+++ src/view/settings/SettingsView.java
@@ -1,6 +1,12 @@
 package view.settings;
 
+import interface_adapter.EraseCity.EraseCityController;
+import interface_adapter.SetCity.SetCityController;
+import interface_adapter.SetDefaultCity.SetDefaultCityController;
+import interface_adapter.SetTempUnit.SetTempUnitController;
+import interface_adapter.SetTimeFormat.SetTimeFormatController;
 import interface_adapter.Settings.SettingsViewModel;
+import interface_adapter.SettingsStart.SettingsStartController;
 
 import javax.swing.*;
 import java.awt.*;
@@ -11,9 +17,9 @@ import java.beans.PropertyChangeListener;
 
 public class SettingsView extends JPanel implements ActionListener, PropertyChangeListener {
     public final String viewName = "settings";
-    public SettingsView(SettingsViewModel settingsViewModel) {
-        // TODO - implement SettingsView.SettingsView
-        this.add(new JLabel("SettingsView"));
+    public SettingsView(SettingsViewModel settingsViewModel, SettingsStartController settingsController, EraseCityController eraseCityController, SetCityController setCityController, SetDefaultCityController setDefaultCityController, SetTempUnitController setTempUnitController, SetTimeFormatController setTimeFormatController) {
+
+
     }
 
     @Override
