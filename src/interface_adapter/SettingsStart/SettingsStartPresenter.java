7c4bf0e0849653bb7a921f8605433ee51dad4a80
EL5 <linedison3@gmail.com>
2023-12-05 03:25:44 -0500

    edited to display correct message

--- src/interface_adapter/SettingsStart/SettingsStartPresenter.java
+++ src/interface_adapter/SettingsStart/SettingsStartPresenter.java
@@ -19,6 +19,7 @@ public class SettingsStartPresenter implements SettingsStartOutputBoundary {
         state.setDefaultCity(outputData.getDefaultCity());
         state.setTempUnit(outputData.getCelsius());
         state.setTimeFormat(outputData.getTime24());
+        state.setDefaultCityMessage("Current default city: " + outputData.getDefaultCity().split(",")[0].substring(0, 1).toUpperCase() + outputData.getDefaultCity().split(",")[0].substring(1) + ", " + outputData.getDefaultCity().split(",")[1].toUpperCase());
         viewModel.setState(state);
         viewModel.firePropertyChanged();
     }
