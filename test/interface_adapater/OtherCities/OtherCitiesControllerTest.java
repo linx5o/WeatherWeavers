5ecf954328b771d2b15f656ca3aa993d973ecf13
junwei0102 <junwei.quan@mail.utoronto.ca>
2023-12-06 06:11:02 +0800

    OtherCitiesControllerTest

---
+++ test/interface_adapater/OtherCities/OtherCitiesControllerTest.java
@@ -0,0 +1,48 @@
+package interface_adapater.OtherCities;
+import interface_adapter.OtherCities.OtherCitiesController;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import use_case.get_other_cities.GetOtherCitiesInputBoundary;
+
+import java.time.LocalTime;
+
+public class OtherCitiesControllerTest {
+
+    private MockGetOtherCitiesInputBoundary mockInputBoundary;
+    private OtherCitiesController controller;
+
+    @Before
+    public void setUp() {
+        // Initialize the stub implementation
+        mockInputBoundary = new MockGetOtherCitiesInputBoundary();
+
+        // Create an instance of OtherCitiesController with the stub
+        controller = new OtherCitiesController(mockInputBoundary);
+    }
+
+    @Test
+    public void testExecute() {
+        // Call the method to test
+        controller.execute();
+
+        assertTrue("Execute method should have been called", mockInputBoundary.wasExecuteCalled());
+    }
+
+    // Stub implementation of GetOtherCitiesInputBoundary
+    private static class MockGetOtherCitiesInputBoundary implements GetOtherCitiesInputBoundary {
+        private boolean executeCalled = false;
+
+        @Override
+        public void execute(LocalTime time) {
+            this.executeCalled = true;
+        }
+
+        public boolean wasExecuteCalled() {
+            return executeCalled;
+        }
+    }
+}
+
+
